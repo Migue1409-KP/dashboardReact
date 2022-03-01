4bc32dfb69fc1baae5380de01cb7e5cf0f9a2d72
Santiago Cifuentes <sacifuentespawk@gmail.com>
2022-03-01 13:18:36 -0500

    total products and categories feed from API

--- src/components/ContentRowMovies.js
+++ src/components/ContentRowMovies.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SmallCard from './SmallCard';
 
 /*  Cada set de datos es un objeto literal */
 
-/* <!-- Movies in DB --> */
+/* <!-- Products in DB --> */
 
-let moviesInDB = {
+let productsInDB = {
     title: 'Productos en base de datos',
     color: 'primary', 
     cuantity: 21,
@@ -14,16 +14,53 @@ let moviesInDB = {
 
 /* <!-- Total awards --> */
 
-let totalAwards = {
-    title:'Total de usuarios', 
+let totalCategories = {
+    title:'Numero de categorías', 
     color:'success', 
-    cuantity: '79',
+    cuantity: 14,
     icon:'fa-award'
 }
 
-let cartProps = [moviesInDB, totalAwards];
+let cartProps = [productsInDB, totalCategories];
 
 function ContentRowMovies(){
+    
+    let [productsInDB, setProductsInDB]= useState([])
+    let [totalCategories, setCategoriesInDB]= useState([])
+    let [cartProps, setCartProps] = useState([])
+    const callApiProducts = async()=>{
+        try {
+            const res = await fetch("https://trueque-online.herokuapp.com/products/api")
+            const result = await res.json()
+            console.log(result)
+            return result
+        } catch (error) {
+            console.error("Caution, there is an error")
+        }
+    }
+
+    useEffect(async()=>{
+        const products = await callApiProducts()
+        
+        setProductsInDB(productsInDB = {
+            title: 'Productos en base de datos',
+            color: 'primary', 
+            cuantity: products.count,
+            icon: 'fa-clipboard-list'
+        })
+        setCategoriesInDB(totalCategories = {
+            title:'Numero de categorías', 
+            color:'success', 
+            cuantity: Object.keys(products.categories).length,
+            icon:'fa-award'
+        })
+        console.log(Object.keys(products.categories).length)
+
+        setCartProps(cartProps = [productsInDB, totalCategories])
+        
+    },[])
+    
+
     return (
     
         <div className="row">
